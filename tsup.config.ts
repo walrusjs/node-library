769b4dd21c63676238fb56e712e4affea6a7dd95
wangxingkang <wang_xingkang@qq.com>
2022-01-19 21:02:31 +0800

    🔨 chore: 使用 tsup 编译

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export const config = defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: true,
+  format: ['cjs', 'esm'],
+  clean: true,
+  dts: true,
+  legacyOutput: true,
+})
